419f365c85fea8f822d772d911e9602faf9a4e8e
Алексей <catdog50rus@gmail.com>
2019-11-07 18:23:26 +0300

    d2

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.slider__box').slick({
+        speed: 500,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+        dots: true,
+        arrows: false,
+    });
+
+});
