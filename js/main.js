70d02c228a1c161490a941613fc036e3d139db5d
Алексей <catdog50rus@gmail.com>
2019-11-07 22:18:31 +0300

    fin

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-$(function () {
-    $('.slider__box').slick({
-        speed: 500,
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        adaptiveHeight: true,
-        dots: true,
-        arrows: false,
-    });
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        margin:10,
+        nav:true,
+        navClass:['slider__butt-left','slider__butt-rigth'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            800:{
+                items:3
+            },
+            1100:{
+                items:4
+            },
+        }
+    })
 
 });
